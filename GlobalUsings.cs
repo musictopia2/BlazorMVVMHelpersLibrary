2421ed46806e6f724cf89da6fbc610593bce6232
Andy <musictopia2@hotmail.com>
2022-01-18 09:20:45 -0700

    Add project files.

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using MessengingHelpers;
+global using Microsoft.AspNetCore.Components;
+global using MVVMFramework.EventModels;
+global using MVVMFramework.Helpers;
+global using MVVMFramework.ViewModels;
+global using gg = MessengingHelpers.MessengingGlobalClass;
---
+++ ParentControl.razor.cs
@@ -0,0 +1,46 @@
+﻿namespace BlazorMVVMHelpersLibrary;
+public partial class ParentControl<VM> : IBlazorParent<VM>, IDisposable
+    where VM : ScreenViewModel
+{
+    private readonly IEventAggregator _aggregator;
+    public ParentControl()
+    {
+        if (gg.Aggregator is null)
+        {
+            gg.SetUpDefaultAggregators(); //this will set up defaults
+        }
+        _aggregator = gg.Aggregator!;
+        Subscribe();
+    }
+    public VM? DataContext { get; set; }
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    private partial void Subscribe();
+    private partial void Unsubscribe();
+    protected override void OnInitialized()
+    {
+        _aggregator.Ask(typeof(VM));
+    }
+    void IHandle<OpenEventModel>.Handle(OpenEventModel message)
+    {
+        DataContext = message.ViewModelUsed as VM;
+        InvokeAsync(() =>
+        {
+            StateHasChanged();
+        });
+    }
+    void IHandle<CloseEventModel>.Handle(CloseEventModel message)
+    {
+        DataContext = null;
+        InvokeAsync(() =>
+        {
+            StateHasChanged();
+        });
+    }
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+    public void Dispose()
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+    {
+        Unsubscribe(); //try this way (?)
+    }
+}
